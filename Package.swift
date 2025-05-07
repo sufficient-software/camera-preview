034db37558f90edeeb6113444d7e5654ff944779
Nick Gartmann <nick@sufficient.software>
2025-05-07 07:07:49 -0500

    Fix issues with Package.swift

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
@@ -6,22 +7,25 @@ let package = Package(
     products: [
         .library(
             name: "CapacitorCameraPreviewPlugin",
-            nargets: ["CameraPreviewPlugin"])
-    ]
+            targets: ["CameraPreviewPlugin"]
+        )
+    ],
     dependencies: [
         .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
     ],
     targets: [
         .target(
             name: "CameraPreviewPlugin",
-            depencencies: [
+            dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
-            ]
-            path: "ios/Plugin")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+            ],
+            path: "ios/Plugin"
+        ),
         .testTarget(
             name: "CameraPreviewPluginTests",
-            dependencies: "CameraPreviewPlugin",
-            path: "ios/PluginTests")
+            dependencies: ["CameraPreviewPlugin"],
+            path: "ios/PluginTests"
+        ),
     ]
 )
